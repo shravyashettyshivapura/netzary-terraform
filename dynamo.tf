c7e0040ef5a2a666327c5f71fa1e9fea64df6a38
shravyashettyshivapura <166837896+shravyashettyshivapura@users.noreply.github.com>
2024-08-31 17:26:54 +0530

    DynamoDB

---
+++ dynamo.tf
@@ -0,0 +1,14 @@
+resource "aws_dynamodb_table" "example" {
+  name         = "example-table"
+  billing_mode  = "PAY_PER_REQUEST"
+  hash_key      = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "example-table"
+  }
+}
