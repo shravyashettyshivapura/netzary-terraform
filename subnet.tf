fe0af3a2399a2a775daa26d5776cdfa6512bbe39
shravyashettyshivapura <166837896+shravyashettyshivapura@users.noreply.github.com>
2024-08-31 17:02:55 +0530

    terraform file to create subnets

---
+++ subnet.tf
@@ -0,0 +1,19 @@
+resource "aws_subnet" "subnet1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet1"
+  }
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-2b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet2"
+  }
+}
